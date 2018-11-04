316bfe8d870356914a588957beadb63d23157621
Antonio Manuel Fresneda Rodríguez <antoniomanuelfr@gmail.com>
2018-11-04 12:36:59 +0100

    gestion errores qr

--- Museo/app/src/main/java/com/p207/npi/Museo/QrFragment.java
+++ Museo/app/src/main/java/com/p207/npi/Museo/QrFragment.java
@@ -2,9 +2,11 @@ package com.p207.npi.Museo;
 
 
 import android.content.Intent;
+import android.icu.text.DateFormat;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +14,8 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
 
 public class QrFragment extends Fragment implements View.OnClickListener{
     private  final int SCAN_REQUEST_CODE = 21;
@@ -23,8 +27,24 @@ public class QrFragment extends Fragment implements View.OnClickListener{
         // Required empty public constructor
     }
 
+    private Map<String, String> processResult(String result){
+
+        Map<String, String> myMap = new HashMap<>();
+        String[] pairs = result.split(",");
+        for (String pair : pairs) {
+            String[] keyValue = pair.split("->");
+            myMap.put(keyValue[0], keyValue[1]);
+        }
+
+        return myMap;
+    }
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent Data){
+
+        String NameKey="NAME";
+        String URLKey="URL";
+
         super.onActivityResult(requestCode, resultCode, Data);
 
         //Se ha escaneado un codigo QR correctamente
@@ -32,11 +52,22 @@ public class QrFragment extends Fragment implements View.OnClickListener{
 
             if (resultCode==QrActivity.RESULT_OK){
                 String result = Data.getStringExtra("result");
-                String Name = result.substring(0,result.indexOf("\n"));
-                String URL = result.substring(result.indexOf("\n"),result.length());
+                Map<String, String> mapResult = processResult(result);
 
-                textViewName.setText(Name);
-                textViewAddress.setText(URL);
+
+                if (mapResult.containsKey(NameKey) && mapResult.containsKey(URLKey)) {
+
+                    String Name = mapResult.get(NameKey);
+
+                    String URL = mapResult.get(URLKey);
+
+                    textViewName.setText(Name);
+                    textViewAddress.setText(URL);
+
+                }else{
+                    Toast.makeText(getActivity(), "QR no valido.", Toast.LENGTH_LONG).show();
+
+                }
 
         //No se escaneo ningun codigo qr
             }else {
