046c8fff1286c41f4d3bfc278a8069bcc18c90d9
Antonio Manuel Fresneda Rodríguez <antoniomanuelfr@gmail.com>
2018-11-09 13:14:40 +0100

    Cosas

--- Museo/app/src/main/java/com/p207/npi/Museo/MainActivity.java
+++ Museo/app/src/main/java/com/p207/npi/Museo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.p207.npi.Museo;
 
 import android.arch.lifecycle.ViewModelProviders;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -8,6 +9,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -19,7 +21,6 @@ public class MainActivity extends AppCompatActivity
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             FragmentManager manager = getSupportFragmentManager();
             FragmentTransaction transaction = manager.beginTransaction();
-
             switch (item.getItemId()) {
 
                 case R.id.navigation_home:
@@ -27,11 +28,13 @@ public class MainActivity extends AppCompatActivity
                     transaction.replace(R.id.mainContainer,new HomeFragment());
                     transaction.commit();
 
+
                     return true;
 
                 case R.id.navigation_qr:
 
                     transaction.replace(R.id.mainContainer,new QrFragment());
+
                     transaction.commit();
 
                     return true;
@@ -42,6 +45,7 @@ public class MainActivity extends AppCompatActivity
                     if (bot == null)
                         bot = new Bot();
                     transaction.replace(R.id.mainContainer, bot);
+
                     transaction.commit();
 
                     return true;
@@ -66,9 +70,6 @@ public class MainActivity extends AppCompatActivity
 
         transaction.replace(R.id.mainContainer,new HomeFragment());
         transaction.commit();
-
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
     @Override
