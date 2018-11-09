f8fc3bf32ec540884c1f5a2aa7ff4ceaeb852eff
Renegeizu <renegeizu@gmail.com>
2018-11-09 23:34:29 +0100

    Sensor Fix

--- Museo/app/src/main/java/com/p207/npi/Museo/MainActivity.java
+++ Museo/app/src/main/java/com/p207/npi/Museo/MainActivity.java
@@ -17,7 +17,6 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
