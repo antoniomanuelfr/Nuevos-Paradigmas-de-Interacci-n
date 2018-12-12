df9c360af0181908b73b5848162af31ad1844b40
alxsm <alesamo_04@hotmail.com>
2018-12-12 20:41:42 +0100

    arreglando la liada de antonio 1.0

---
+++ MuseoGoTKinectv1.8/MainWindow.xaml.cs
@@ -0,0 +1,225 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="MainWindow.xaml.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+namespace Microsoft.Samples.Kinect.GOT
+{
+    using System;
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Data;
+    using Microsoft.Kinect;
+    using Microsoft.Kinect.Toolkit;
+    using Microsoft.Kinect.Toolkit.Controls;
+    using Microsoft.Samples.Kinect.ControlsBasics;
+
+
+    /// <summary>
+    /// Interaction logic for MainWindow
+    /// </summary>
+    public partial class MainWindow
+    {
+
+        /// <summary>
+        /// Active Kinect sensor
+        /// </summary>
+        private KinectSensor sensor;
+        private fit mov;
+
+        private const double ScrollErrorMargin = 0.001;
+
+        private const int PixelScrollByAmount = 20;
+
+        private readonly KinectSensorChooser sensorChooser;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class. 
+        /// </summary>
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            mov = new fit();
+
+            // initialize the sensor chooser and UI
+            this.sensorChooser = new KinectSensorChooser();
+            this.sensorChooser.KinectChanged += SensorChooserOnKinectChanged;
+            this.sensorChooserUi.KinectSensorChooser = this.sensorChooser;
+            this.sensorChooser.Start();
+
+            // Bind the sensor chooser's current sensor to the KinectRegion
+            var regionSensorBinding = new Binding("Kinect") { Source = this.sensorChooser };
+            BindingOperations.SetBinding(this.kinectRegion, KinectRegion.KinectSensorProperty, regionSensorBinding);
+
+        }
+        /// <summary>
+        /// Execute startup tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void WindowLoaded(object sender, RoutedEventArgs e)
+        {
+
+            // Look through all sensors and start the first connected one.
+            // This requires that a Kinect is connected at the time of app startup.
+            // To make your app robust against plug/unplug, 
+            // it is recommended to use KinectSensorChooser provided in Microsoft.Kinect.Toolkit (See components in Toolkit Browser).
+            foreach (var potentialSensor in KinectSensor.KinectSensors)
+            {
+                if (potentialSensor.Status == KinectStatus.Connected)
+                {
+                    this.sensor = potentialSensor;
+                    break;
+                }
+            }
+
+            if (null != this.sensor)
+            {
+                // Turn on the skeleton stream to receive skeleton frames
+                this.sensor.SkeletonStream.Enable();
+
+                // Add an event handler to be called whenever there is new color frame data
+                this.sensor.SkeletonFrameReady += this.SensorSkeletonFrameReady;
+
+                this.sensor.SkeletonStream.TrackingMode = SkeletonTrackingMode.Default;
+                // Start the sensor!
+                try
+                {
+                    this.sensor.Start();
+                }
+                catch (IOException)
+                {
+                    this.sensor = null;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Execute shutdown tasks
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        private void WindowClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (null != this.sensor)
+            {
+                this.sensor.Stop();
+            }
+        }
+        
+        
+        /// <summary>
+        /// Event handler for Kinect sensor's SkeletonFrameReady event
+        /// </summary>
+        /// <param name="sender">object sending the event</param>
+        /// <param name="e">event arguments</param>
+        /// Aqui va los gestos
+        /// 
+        private void SensorSkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            Skeleton[] skeletons = new Skeleton[0];
+
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame())
+            {
+                if (skeletonFrame != null)
+                {
+                    skeletons = new Skeleton[skeletonFrame.SkeletonArrayLength];
+                    skeletonFrame.CopySkeletonDataTo(skeletons);
+                }
+            }
+            if (skeletons.Length != 0)
+            {
+                foreach (Skeleton skel in skeletons)
+                {
+                    if (skel.TrackingState == SkeletonTrackingState.Tracked)
+                    {
+
+                        //Gestures(skel);
+                        if (mov.deteccion(skel))
+                        {
+                            left.Opacity = (left.Opacity+1)%2;
+                        }
+                    }
+
+                }
+            }
+
+
+        }
+
+        ///  <sumary>
+        /// Gestures detection
+        /// </sumary>
+        /// <param> </param>
+        /// 
+        private void Gestures(Skeleton sk)
+        {
+        }    
+
+        /// <summary>
+        /// Called when the KinectSensorChooser gets a new sensor
+        /// </summary>
+        /// <param name="sender">sender of the event</param>
+        /// <param name="args">event arguments</param>
+        private static void SensorChooserOnKinectChanged(object sender, KinectChangedEventArgs args)
+        {
+            if (args.OldSensor != null)
+            {
+                try
+                {
+                    args.OldSensor.DepthStream.Range = DepthRange.Default;
+                    args.OldSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                    args.OldSensor.DepthStream.Disable();
+                    args.OldSensor.SkeletonStream.Disable();
+                }
+                catch (InvalidOperationException)
+                {
+                    // KinectSensor might enter an invalid state while enabling/disabling streams or stream features.
+                    // E.g.: sensor might be abruptly unplugged.
+                }
+            }
+
+            if (args.NewSensor != null)
+            {
+                try
+                {
+                    args.NewSensor.DepthStream.Enable(DepthImageFormat.Resolution640x480Fps30);
+                    args.NewSensor.SkeletonStream.Enable();
+
+                    try
+                    {
+                        //args.NewSensor.DepthStream.Range = DepthRange.Near;
+                        args.NewSensor.SkeletonStream.EnableTrackingInNearRange = true;
+                    }
+                    catch (InvalidOperationException)
+                    {
+                        // Non Kinect for Windows devices do not support Near mode, so reset back to default mode.
+                        args.NewSensor.DepthStream.Range = DepthRange.Default;
+                        args.NewSensor.SkeletonStream.EnableTrackingInNearRange = false;
+                    }
+                }
+                catch (InvalidOperationException)
+                {
+                    // KinectSensor might enter an invalid state while enabling/disabling streams or stream features.
+                    // E.g.: sensor might be abruptly unplugged.
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handle a button click from the wrap panel.
+        /// </summary>
+        /// <param name="sender">Event sender</param>
+        /// <param name="e">Event arguments</param>
+        private void KinectTileButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.Content = new Westeros();
+        }
+        private void KinectTileButtonClick2(object sender, RoutedEventArgs e)
+        {
+            this.Content = new Essos();
+        }
+    }
+}
---
+++ MuseoGoTKinectv1.8/Movimiento19.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Microsoft.Samples.Kinect.ControlsBasics
+{
+    class Movimiento19
+    {
+
+
+        private int fase;
+        private float factorA, factorB;
+
+        public Movimiento19(float a = 1.3f, float b = 0.7f)
+        {
+            fase = 0;
+            factorA = a;
+            factorB = b;
+        }
+
+        public int getFase()
+        {
+            return fase;
+        }
+        public void setFase(int a)
+        {
+            fase=a;
+        }
+
+        private bool Reposo(Skeleton esqueleto)
+        {
+            Joint[] puntos = new Joint[3];
+            //Tomamos los 6 puntos de referencia para comprobar el estado de reposo
+            puntos[0] = esqueleto.Joints[JointType.ShoulderRight];
+            puntos[1] = esqueleto.Joints[JointType.ElbowRight];
+            puntos[2] = esqueleto.Joints[JointType.WristRight];
+            if(puntos[2].Position.Y > puntos[1].Position.Y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            /*if ((puntos[0].Position.Y < (puntos[1].Position.Y * factorA)) && (puntos[0].Position.Y > (puntos[1].Position.Y * factorB))) {
+                if ((puntos[0].Position.Z < (puntos[1].Position.Z * factorA)) && (puntos[0].Position.Z > (puntos[1].Position.Z * factorB))) {
+                    if ((puntos[2].Position.X < (puntos[0].Position.X * factorA)) && (puntos[2].Position.X > (puntos[0].Position.X * factorB))) {
+                        if ((puntos[2].Position.Z < (puntos[0].Position.X * factorA)) && (puntos[2].Position.Z > (puntos[0].Position.X * factorB))) {
+                            return true;
+                        }
+                        else { return true; }
+                    }
+                    else { return true; }
+                }
+                else { return true; }
+            }
+            else
+            {
+                return false;
+            }*/
+
+        }
+        public bool movimiento(Skeleton esqueleto)
+        {
+            Joint[] puntos = new Joint[3];
+            //Tomamos los 3 puntos de referencia para comprobar el movimiento
+            puntos[0] = esqueleto.Joints[JointType.ShoulderRight];
+            puntos[1] = esqueleto.Joints[JointType.ElbowRight];
+            puntos[2] = esqueleto.Joints[JointType.WristRight];
+            if( fase ==0) {
+                if (Reposo(esqueleto))
+                {
+                    fase = 1;
+                    Console.WriteLine("111111111");
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else if (fase == 1) {
+                if(puntos[2].Position.Y > puntos[1].Position.Y)
+                {
+                    Console.WriteLine("111111111");
+                    return true;
+                }
+                else if ((puntos[2].Position.Y < puntos[1].Position.Y)){
+                    fase = 2;
+                    Console.WriteLine("222222222");
+                    return true;
+                }
+                else{
+                    return false;
+                }
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
